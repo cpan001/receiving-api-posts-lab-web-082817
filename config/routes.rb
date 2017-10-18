0efa04b4b59d58ddba691a9de9e68f767013b99b
cpan001 <github email address>
2017-10-18 11:45:39 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
-  resources :products, only: [:show, :index, :new, :create]
+  resources :products, only: [:show, :index, :new]
+  post "/products/new", to: "products#create", as: "new_product_path"
 end
